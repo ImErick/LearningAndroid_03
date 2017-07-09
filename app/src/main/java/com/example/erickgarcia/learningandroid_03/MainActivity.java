79295eec75b21dffcfc6b3ae7b07339bef3666b4
ImErick <3r1ck.442@gmail.com>
2017-07-09 11:19:22 -0500

    finish this fucking recycler adapter D:<

--- app/src/main/java/com/example/erickgarcia/learningandroid_03/MainActivity.java
+++ app/src/main/java/com/example/erickgarcia/learningandroid_03/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.erickgarcia.learningandroid_03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> names;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        names = this.getAllNames();
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new MyAdapter(names, R.layout.recycle_view, new MyAdapter.OnClickListener() {
+            @Override
+            public void onItemClick(String name, int position) {
+                Toast.makeText(MainActivity.this, "name: "+name+", position: "+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<String> getAllNames(){
+        return new ArrayList<String>(){
+            {
+                add("erick");
+                add("monse");
+                add("abril");
+            }
+        };
     }
 }
