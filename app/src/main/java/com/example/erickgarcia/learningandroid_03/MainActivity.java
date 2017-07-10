ea86d70e48afbf5725df0319ca5e249ab591ff6e
ImErick <3r1ck.442@gmail.com>
2017-07-09 22:21:07 -0500

    implement add and delete

--- app/src/main/java/com/example/erickgarcia/learningandroid_03/MainActivity.java
+++ app/src/main/java/com/example/erickgarcia/learningandroid_03/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.erickgarcia.learningandroid_03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +31,45 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MyAdapter(names, R.layout.recycle_view, new MyAdapter.OnClickListener() {
             @Override
             public void onItemClick(String name, int position) {
-                Toast.makeText(MainActivity.this, "name: "+name+", position: "+position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "You're delete: "+name+", position: "+position, Toast.LENGTH_SHORT).show();
+                deleteName(position);
             }
         });
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+    }
+
+    // maneja eventos onClick
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_name:
+                this.addName(0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void addName(int position) {
+        this.names.add(position, "New name " +(++counter));
+        adapter.notifyItemInserted(position);
+        layoutManager.scrollToPosition(position);
+    }
+
+    private void deleteName(int position){
+        this.names.remove(position);
+        adapter.notifyItemRemoved(position);
+    }
+
+    // crea
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
     private List<String> getAllNames(){
